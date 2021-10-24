e2adaaebe34f2ad58271cec5e64bf7348ee27d80
Weheineman <gi4nn1@yahoo.de>
2021-10-24 14:17:52 -0300

    Black formatting

--- TP1/variable_selection/data.py
+++ TP1/variable_selection/data.py
@@ -1,6 +1,7 @@
 from typing import Any, List
 import random
 
+
 class Data:
     def __init__(self, file_stem: str):
         self.file_stem = file_stem
@@ -11,27 +12,27 @@ class Data:
         self.train_results = []
 
     def read_train_data(self):
-        '''
+        """
         Reads train data from file "file_stem.data".
-        '''
+        """
         self.__read_data(
             f"{self.file_stem}.data", self.train_values, self.train_results
         )
 
     def read_test_data(self):
-        '''
+        """
         Reads test data from file "file_stem.test".
-        '''
+        """
         self.__read_data(f"{self.file_stem}.test", self.test_values, self.test_results)
 
-    def make_test_from_train(self, test_ratio: float):
-        '''
+    def make_test_from_train(self, test_ratio: float = 0.2):
+        """
         Separates a random subsample from the train data to use as test data.
         The size of the test data is test_ratio * size of train data.
-        '''
+        """
 
         if test_ratio < 0 or test_ratio > 1:
-            raise Exception('test_ratio needs to be a float between 0 and 1.')
+            raise Exception("test_ratio needs to be a float between 0 and 1.")
         test_len = int(len(self.train_values) * test_ratio)
 
         # Random shuffle train data.
@@ -45,17 +46,16 @@ class Data:
         self.train_values = self.train_values[test_len:]
         self.train_results = self.train_results[test_len:]
 
-
     def __read_data(self, filename: str, values: List[Any], results: List[Any]):
-        '''
+        """
         Reads float values for the value list and a str for the result (a class).
-        '''
+        """
         file = open(filename, "r")
         values.clear()
         results.clear()
 
-        self.variable_names = file.readline().replace('"', '').split()[:-1]
+        self.variable_names = file.readline().replace('"', "").split()[:-1]
         for line in file.readlines():
-            input_list = [float(value) for value in line.replace('"', '').split()]
+            input_list = [float(value) for value in line.replace('"', "").split()]
             values.append(input_list[:-1])
             results.append(str(input_list[-1]))
