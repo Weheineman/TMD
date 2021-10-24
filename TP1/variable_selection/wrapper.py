1ba374a7af21e772ddbdff718a30576b39b4c430
Weheineman <gi4nn1@yahoo.de>
2021-10-24 14:18:48 -0300

    Wrapper bugfixing. * Replaced '__' with '_' for "private" method naming convention.

--- TP1/variable_selection/wrapper.py
+++ TP1/variable_selection/wrapper.py
@@ -1,9 +1,11 @@
 from abc import ABC, abstractmethod
 from typing import List
 
+import numpy as np
 from sklearn.model_selection import cross_val_score
 
-from data import Data
+from variable_selection.data import Data
+
 
 class GreedyWrapper(ABC):
     def __init__(self, model, data: Data):
@@ -13,30 +15,40 @@ class GreedyWrapper(ABC):
 
     def select_variables(self):
         """Selects a subset of variables and trains the model with them."""
-        while self.__step():
+        while self._step():
             pass
 
-        self.__fit_model()
+        self._fit_model()
 
-    def __estimate_error(self, variables: List[bool]) -> float:
+    def selected_variables(self):
+        return [
+            name for name, used in zip(self.data.variable_names, self.state) if used
+        ]
+
+    def _estimate_error(self, variables: List[bool]) -> float:
         """
         Trains the model with the selected variables and returns the estimated error using 5-fold CV.
         """
-        return cross_val_score(
-            self.model,
-            self.__filter_variables(self.data.train_values, variables),
-            self.data.train_results,
+        # If no variables are selected, return 1 as the error.
+        if not any(variables):
+            return 1
+
+        return np.mean(
+            cross_val_score(
+                self.model,
+                self._filter_variables(self.data.train_values, variables),
+                self.data.train_results,
+            )
         )
 
-    def __fit_model(self) -> float:
+    def _fit_model(self) -> float:
         """Fits the model using the selected variables on all the train data."""
         return self.model.fit(
-            self.__filter_variables(self.data.train_values, self.state),
+            self._filter_variables(self.data.train_values, self.state),
             self.data.train_results,
         )
 
-    # TODO: Maybe always apply to train_values?
-    def __filter_variables(
+    def _filter_variables(
         self, data: List[List[bool]], variables: List[bool]
     ) -> List[List[bool]]:
         """Returns a copy of the data with only the variables selected."""
@@ -45,7 +57,7 @@ class GreedyWrapper(ABC):
         ]
 
     @abstractmethod
-    def __step(self):
+    def _step(self) -> bool:
         """
         Returns True if adding a variable improves the error, False otherwise.
         Updates the state and predicted_error if a step is taken.
@@ -54,22 +66,20 @@ class GreedyWrapper(ABC):
 
 
 class ForwardWrapper(GreedyWrapper):
-    def __init__(self, model, data):
+    def __init__(self, model, data: Data):
         super().__init__(model, data)
-        self.state = [False] * data.dimension
+        self.state = [False] * len(data.variable_names)
         self.predicted_error = 1
 
-    def __step(self) -> bool:
+    def _step(self) -> bool:
         lowest_error = self.predicted_error
         best_step = self.state
-        unused_vars = [
-            index for index, used in enumerate(self.current_state) if not used
-        ]
+        unused_vars = [index for index, used in enumerate(self.state) if not used]
 
         for unused_index in unused_vars:
-            next_state = self.current_state.copy()
+            next_state = self.state.copy()
             next_state[unused_index] = True
-            predicted_error = self.__estimate_error(next_state)
+            predicted_error = self._estimate_error(next_state)
             if predicted_error < lowest_error:
                 lowest_error = predicted_error
                 best_step = next_state
@@ -84,20 +94,20 @@ class ForwardWrapper(GreedyWrapper):
 
 
 class BackwardWrapper(GreedyWrapper):
-    def __init__(self, model, data):
+    def __init__(self, model, data: Data):
         super().__init__(model, data)
-        self.state = [True] * data.dimension
-        self.predicted_error = self.__estimate_error(self.state)
+        self.state = [True] * len(data.variable_names)
+        self.predicted_error = self._estimate_error(self.state)
 
-    def __step(self) -> bool:
+    def _step(self) -> bool:
         lowest_error = self.predicted_error
         best_step = self.state
-        used_vars = [index for index, used in enumerate(self.current_state) if used]
+        used_vars = [index for index, used in enumerate(self.state) if used]
 
         for used_index in used_vars:
-            next_state = self.current_state.copy()
+            next_state = self.state.copy()
             next_state[used_index] = False
-            predicted_error = self.__estimate_error(next_state)
+            predicted_error = self._estimate_error(next_state)
             if predicted_error < lowest_error:
                 lowest_error = predicted_error
                 best_step = next_state
