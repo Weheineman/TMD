3a890b873f1c246962be53489817ed0724fe55ad
Weheineman <gi4nn1@yahoo.de>
2021-10-24 23:38:52 -0300

    Added missing '_' to method names.

--- TP1/variable_selection/wrapper.py
+++ TP1/variable_selection/wrapper.py
@@ -15,10 +15,10 @@ class GreedyWrapper(ABC):
 
     def select_variables(self):
         """Selects a subset of variables and trains the model with them."""
-        while self._step():
+        while self._step_():
             pass
 
-        self._fit_model()
+        self._fit_model_()
 
     def selected_variables(self) -> List[str]:
         """Returns the name of the selected variables."""
@@ -26,7 +26,7 @@ class GreedyWrapper(ABC):
             name for name, used in zip(self.data.variable_names, self.state) if used
         ]
 
-    def _estimate_error(self, variables: List[bool]) -> float:
+    def _estimate_error_(self, variables: List[bool]) -> float:
         """
         Trains the model with the selected variables and returns the estimated error using 5-fold CV.
         """
@@ -37,19 +37,19 @@ class GreedyWrapper(ABC):
         return np.mean(
             cross_val_score(
                 self.model,
-                self._filter_variables(self.data.train_values, variables),
+                self._filter_variables_(self.data.train_values, variables),
                 self.data.train_results,
             )
         )
 
-    def _fit_model(self) -> float:
+    def _fit_model_(self) -> float:
         """Fits the model using the selected variables on all the train data."""
         return self.model.fit(
-            self._filter_variables(self.data.train_values, self.state),
+            self._filter_variables_(self.data.train_values, self.state),
             self.data.train_results,
         )
 
-    def _filter_variables(
+    def _filter_variables_(
         self, data: List[List[bool]], variables: List[bool]
     ) -> List[List[bool]]:
         """Returns a copy of the data with only the variables selected."""
@@ -58,9 +58,9 @@ class GreedyWrapper(ABC):
         ]
 
     @abstractmethod
-    def _step(self) -> bool:
+    def _step_(self) -> bool:
         """
-        Returns True if adding a variable improves the error, False otherwise.
+        Returns True if changing the state by one variable improves the error, False otherwise.
         Updates the state and predicted_error if a step is taken.
         """
         pass
@@ -72,7 +72,7 @@ class ForwardWrapper(GreedyWrapper):
         self.state = [False] * len(data.variable_names)
         self.predicted_error = 1
 
-    def _step(self) -> bool:
+    def _step_(self) -> bool:
         lowest_error = self.predicted_error
         best_step = self.state
         unused_vars = [index for index, used in enumerate(self.state) if not used]
@@ -80,7 +80,7 @@ class ForwardWrapper(GreedyWrapper):
         for unused_index in unused_vars:
             next_state = self.state.copy()
             next_state[unused_index] = True
-            predicted_error = self._estimate_error(next_state)
+            predicted_error = self._estimate_error_(next_state)
             if predicted_error < lowest_error:
                 lowest_error = predicted_error
                 best_step = next_state
@@ -98,9 +98,9 @@ class BackwardWrapper(GreedyWrapper):
     def __init__(self, model, data: Data):
         super().__init__(model, data)
         self.state = [True] * len(data.variable_names)
-        self.predicted_error = self._estimate_error(self.state)
+        self.predicted_error = self._estimate_error_(self.state)
 
-    def _step(self) -> bool:
+    def _step_(self) -> bool:
         lowest_error = self.predicted_error
         best_step = self.state
         used_vars = [index for index, used in enumerate(self.state) if used]
@@ -108,7 +108,7 @@ class BackwardWrapper(GreedyWrapper):
         for used_index in used_vars:
             next_state = self.state.copy()
             next_state[used_index] = False
-            predicted_error = self._estimate_error(next_state)
+            predicted_error = self._estimate_error_(next_state)
             if predicted_error < lowest_error:
                 lowest_error = predicted_error
                 best_step = next_state
